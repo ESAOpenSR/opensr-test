d7bf03b6cdcfbd422e8ad3501d17107edcf2b138
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2024-02-09 12:46:31 +0100

    new version of lightblue

--- opensr_test/lightglue/__init__.py
+++ opensr_test/lightglue/__init__.py
@@ -1,4 +1,7 @@
-from .lightglue import LightGlue
-from .superpoint import SuperPoint
-from .disk import DISK
-from .utils import match_pair
+from .aliked import ALIKED  # noqa
+from .disk import DISK  # noqa
+from .dog_hardnet import DoGHardNet  # noqa
+from .lightglue import LightGlue  # noqa
+from .sift import SIFT  # noqa
+from .superpoint import SuperPoint  # noqa
+from .utils import match_pair  # noqa
--- opensr_test/lightglue/disk.py
+++ opensr_test/lightglue/disk.py
@@ -1,13 +1,10 @@
-from types import SimpleNamespace
-
 import kornia
 import torch
-import torch.nn as nn
 
-from .utils import ImagePreprocessor
+from .utils import Extractor
 
 
-class DISK(nn.Module):
+class DISK(Extractor):
     default_conf = {
         "weights": "depth",
         "max_num_keypoints": None,
@@ -18,7 +15,6 @@ class DISK(nn.Module):
     }
 
     preprocess_conf = {
-        **ImagePreprocessor.default_conf,
         "resize": 1024,
         "grayscale": False,
     }
@@ -26,16 +22,16 @@ class DISK(nn.Module):
     required_data_keys = ["image"]
 
     def __init__(self, **conf) -> None:
-        super().__init__()
-        self.conf = {**self.default_conf, **conf}
-        self.conf = SimpleNamespace(**self.conf)
+        super().__init__(**conf)  # Update with default configuration.
         self.model = kornia.feature.DISK.from_pretrained(self.conf.weights)
 
     def forward(self, data: dict) -> dict:
-        """ Compute keypoints, scores, descriptors for image """
+        """Compute keypoints, scores, descriptors for image"""
         for key in self.required_data_keys:
             assert key in data, f"Missing key {key} in data"
         image = data["image"]
+        if image.shape[1] == 1:
+            image = kornia.color.grayscale_to_rgb(image)
         features = self.model(
             image,
             n=self.conf.max_num_keypoints,
@@ -53,19 +49,7 @@ class DISK(nn.Module):
         descriptors = torch.stack(descriptors, 0)
 
         return {
-            "keypoints": keypoints.to(image),
-            "keypoint_scores": scores.to(image),
-            "descriptors": descriptors.to(image),
+            "keypoints": keypoints.to(image).contiguous(),
+            "keypoint_scores": scores.to(image).contiguous(),
+            "descriptors": descriptors.to(image).contiguous(),
         }
-
-    def extract(self, img: torch.Tensor, **conf) -> dict:
-        """ Perform extraction with online resizing"""
-        if img.dim() == 3:
-            img = img[None]  # add batch dim
-        assert img.dim() == 4 and img.shape[0] == 1
-        shape = img.shape[-2:][::-1]
-        img, scales = ImagePreprocessor(**{**self.preprocess_conf, **conf})(img)
-        feats = self.forward({"image": img})
-        feats["image_size"] = torch.tensor(shape)[None].to(img).float()
-        feats["keypoints"] = (feats["keypoints"] + 0.5) / scales[None] - 0.5
-        return feats
